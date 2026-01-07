72ff1718038eaea4fc288f6a1d7b38718e547850
THARUNJP <tharunroshini71@gmail.com>
2026-01-07 21:14:06 +0530

    ciunter logic added

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,14 +1,21 @@
-const clients = new Map();// to keep track of each clients
+const clients = new Map(); // to keep track of each clients
 export async function SSE(req, res) {
   //set header to keep connection alive
-  res.setHeaders("Content-type", "text/event-stream");
+  res.setHeader("Content-type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
+  // res.write(`data: Connected to server\n\n`);
+
+  let count = 0;
+  const IntervalId = setInterval(() => {
+    res.write(`data: ${count}\n\n`);
+    count++;
+  }, 2000);
 
   // this writes to all connected clients
-  res.write(`data: Connected to server\n\n`);
 
   req.on("close", () => {
+    clearInterval(IntervalId);
     res.end();
   });
 }
